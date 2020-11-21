f6b34b64b063852c381b87907d5f2ce251792a21
Zelacks <aciertz@gmail.com>
2020-11-21 14:21:40 +0800

    inital commit

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyProduct("Sandbox")]
+[assembly: AssemblyCompany("Zelacks")]
+[assembly: AssemblyCopyright("Copyright © 2020 Zelacks")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyTitle("Sandbox")]
+[assembly: AssemblyDescription("")]
+//[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("FE82077D-EA92-4DAA-9219-7BEFA6060CD2")]
+
+// Version information for an assembly consists of the following four values:
+// Major Version
+// Minor Version
+// Build Number
+// Revision
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyInformationalVersion("VERSIONSTRING")]
---
+++ Source/Source/SandboxModBase.cs
@@ -0,0 +1,10 @@
+using HugsLib;
+using Zelacks.ZelShared;
+
+namespace Zelacks.Sandbox
+{
+    internal class SandboxModBase : ModBase
+    {
+        public override string ModIdentifier => ModNames.ZelSandboxMod;
+    }
+}
